373cd8e76d4f4b5dd915cec882515211b7953307
Алексей Лапшов <avlapshov@dasreda.ru>
2020-01-17 10:18:39 +0400

    Mirror successed

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,13 +8,10 @@ import servlets.MirrorServlet;
 public class Main {
     public static void main(String[] args) throws Exception {
         MirrorServlet mirrorRequestsServlet = new MirrorServlet();
-
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.addServlet(new ServletHolder(mirrorRequestsServlet), "/*");
-
         Server server = new Server(8080);
         server.setHandler(context);
-
         server.start();
         server.join();
     }
--- src/main/java/servlets/MirrorServlet.java
+++ src/main/java/servlets/MirrorServlet.java
@@ -1,23 +1,42 @@
 package servlets;
 
 import templater.PageGenerator;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class MirrorServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response){
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Map<String, Object> pageVariables = createPageVariablesMap(request);
-        pageVariables.put("mirror", "");
-
-        response.getWriter().println(PageGenetator.instance().getPage("page.html", pageVariables));
-
+        pageVariables.put("key", "");
+        response.getWriter().println(PageGenerator.instance().getPage("page.html", pageVariables));
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
 
+    }
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+        Map<String, Object> pageVariables = createPageVariablesMap(request);
+
+        String key = request.getParameter("key");
+
+        response.setContentType("text/html;charset=utf-8");
+
+        if (key == null || key.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        } else {
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+        pageVariables.put("key", key == null ? "" : key);
+
+        response.getWriter().println(PageGenerator.instance().getPage("page.html", pageVariables));
     }
 
     private static Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
